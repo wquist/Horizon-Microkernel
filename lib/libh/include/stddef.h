bc1bedcecfdd73a849b041e08e131106fbb50190
Wyatt Lindquist <git.wquist@gmail.com>
2014-06-15 12:11:11 -0400

    Forgot to define size_t in stddef.h.

--- lib/libh/include/stddef.h
+++ lib/libh/include/stddef.h
@@ -3,6 +3,14 @@
 
 #include <features.h>
 
+#ifdef __GNUC__
+typedef __SIZE_TYPE__    size_t;
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#else
+typedef unsigned long size_t;
+typedef long ptrdiff_t;
+#endif
+
 #define NULL ((void*)0L)
 
 #endif
