e19ed83fe43a2646722caa72a4e5693845adbcd2
Wyatt Lindquist <git.wquist@gmail.com>
2014-06-19 19:26:09 -0400

    Added address align helper functions.

---
+++ kernel/util/addr.h
@@ -0,0 +1,37 @@
+/* Horizon Microkernel - Hobby Operating System
+ * Copyright (C) 2014 Wyatt Lindquist
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*! \file util/addr.h
+ *  \date June 2014
+ */
+
+#pragma once
+
+#include <debug/error.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static inline uintptr_t addr_align(uintptr_t addr, size_t bound)
+{
+	dassert(bound && (bound & (bound-1)) == 0); //< Make sure bound is a power of two.
+	return (addr & ~(bound-1));
+}
+
+static inline uintptr_t addr_align_next(uintptr_t addr, size_t bound)
+{
+	return addr_align(addr + bound-1, bound);
+}
