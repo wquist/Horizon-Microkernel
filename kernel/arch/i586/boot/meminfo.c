2956832f3e9a44cc276b23daa9ecc85a700353f2
Wyatt Lindquist <git.wquist@gmail.com>
2014-06-20 01:50:29 -0400

    Wrote functions to keep track of memory info.

---
+++ kernel/arch/i586/boot/meminfo.c
@@ -0,0 +1,95 @@
+/* Horizon Microkernel - Hobby Operating System
+ * Copyright (C) 2014 Wyatt Lindquist
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "meminfo.h"
+#include <memory/region.h>
+#include <debug/log.h>
+#include <util/compare.h>
+
+static size_t mem_total    = 0;
+static size_t mem_usable   = 0;
+static uintptr_t mem_limit = 0;
+
+static size_t mmap_count = 0;
+static meminfo_mmap_t* mmaps = NULL;
+
+//! Read the memory and mmap information from the multiboot structure.
+void meminfo_init(const multiboot_info_t* mbi)
+{
+	mem_total = multiboot_mem_size_get();
+	dassert(mem_total != 0);
+	dtrace("Total memory: %iKB. Memory map: ", mem_total / 1024);
+
+	mmaps = region_reserve(0, true); //< Get the start address with an empty reserve.
+
+	multiboot_mmap_t* entry = multiboot_mmap_get(mbi, NULL);
+	for (; entry; entry = multiboot_mmap_get(mbi, entry))
+	{
+		// Adding these two shouldn't have a chance to overflow.
+		uintptr_t end = entry->addr + entry->length;
+
+		dprintf("\t[%2i] ", entry->type);
+		dprintf("%#8X - %#8X\n", entry->addr, end);
+
+		// Otherwise this memory is reserved or damaged.
+		if (entry->type != MULTIBOOT_MEMORY_AVAILABLE)
+			continue;
+
+		mem_usable += entry->length;
+		mem_limit = max(mem_limit, end);
+
+		meminfo_mmap_t* curr = (meminfo_mmap_t*)region_reserve(sizeof(meminfo_mmap_t), false);
+		curr->start = entry->addr;
+		curr->end   = end;
+
+		++mmap_count;
+	}
+}
+
+//! Check if the given range lies completely in available memory.
+/*! This assumes each mmap entry is not adjacent; entries should not be fragmented. */
+bool meminfo_region_is_valid(uintptr_t start, size_t size)
+{
+	// Check for overflow
+	dassert(start <= LONG_MAX - size);
+
+	uintptr_t end = start + size;
+	for (size_t i = 0; i != mmap_count; ++i)
+	{
+		if (mmaps[i].start <= start && mmaps[i].end > end)
+			return true;
+	}
+
+	return false;
+}
+
+//! Get the combined size of all types of memory available.
+size_t meminfo_total_get() { return mem_total; }
+//! Get the sum of all usable regions of memory.
+size_t meminfo_usable_get() { return mem_usable; }
+//! Get the highest usable memory address.
+uintptr_t meminfo_limit_get() { return mem_limit; }
+
+//! Get the number of available memory regions.
+size_t meminfo_mmap_count_get() { return mmap_count; }
+
+//! Returns the mmap range info for the given index.
+const meminfo_mmap_t* meminfo_mmap_get(size_t index)
+{
+	dassert(index < mmap_count);
+	return &(mmaps[index]);
+}
---
+++ kernel/arch/i586/boot/meminfo.h
@@ -0,0 +1,46 @@
+/* Horizon Microkernel - Hobby Operating System
+ * Copyright (C) 2014 Wyatt Lindquist
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*! \file arch/i586/boot/meminfo.h
+ *  \date June 2014
+ */
+
+#pragma once
+
+#include <spec/multiboot.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+//! A section of available memory
+typedef struct meminfo_mmap meminfo_mmap_t;
+struct meminfo_mmap
+{
+	uintptr_t start;
+	uintptr_t end;
+};
+
+void meminfo_init(const multiboot_info_t* mbi);
+
+bool meminfo_region_is_valid(uintptr_t start, size_t size);
+
+size_t meminfo_total_get();
+size_t meminfo_usable_get();
+uintptr_t meminfo_limit_get();
+
+size_t meminfo_mmap_count_get();
+const meminfo_mmap_t* meminfo_mmap_get(size_t index);
