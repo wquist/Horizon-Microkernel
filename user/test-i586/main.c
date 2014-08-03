9e556340581601f68b8d890fedcb24a156736882
Wyatt Lindquist <git.wquist@gmail.com>
2014-08-03 18:34:19 -0400

    Testing dispatch call.

--- user/test-i586/main.c
+++ user/test-i586/main.c
@@ -3,23 +3,35 @@
 #include <stdint.h>
 #include <memory.h>
 
-int vmap(void* dest, void* phys, size_t size) { int ret; __asm("int $0x95" : "=a" (ret) : "a" (8), "b" (dest), "c" (phys), "d" (size)); return ret; }
+int dispatch(void* entry, void* stack) { int ret; __asm("int $0x95" : "=a" (ret) : "a" (2), "b" (entry), "c" (stack)); return ret; }
+int vmap(void* dest, size_t size) { int ret; __asm("int $0x95" : "=a" (ret) : "a" (8), "b" (dest), "c" (size)); return ret; }
+int pmap(void* dest, void* phys, size_t size) { int ret; __asm("int $0x95" : "=a" (ret) : "a" (8), "b" (dest), "c" (phys), "d" (size)); return ret; }
 
 uint16_t* const video_mem = (void*)0xA0000000;
 size_t cursor;
 
+void async();
 void print(const char* s);
 
 void main()
 {
-	int res = vmap(video_mem, (void*)0xB8000, 4096);
-	if (res != 0)
-		goto done;
-
+	pmap(video_mem, (void*)0xB8000, 4096);
 	memset(video_mem, 0, 80*25*sizeof(uint16_t));
+
 	print("Hello.");
 
-done:
+	vmap((void*)0xB0000000, 4096);
+	dispatch(async, (void*)(0xB0000000+4095));
+
+	print("Hello again.");
+
+	for (;;);
+}
+
+void async()
+{
+	print("Hello from thread.");
+
 	for (;;);
 }
 
