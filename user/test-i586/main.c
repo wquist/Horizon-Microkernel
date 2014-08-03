8c8c0e56c8d80bb950339ca1e9bf7521ad9f6319
Wyatt Lindquist <git.wquist@gmail.com>
2014-08-03 18:14:04 -0400

    Started to test vmap call.

--- user/test-i586/main.c
+++ user/test-i586/main.c
@@ -1,9 +1,36 @@
 // main.c
 
+#include <stdint.h>
+#include <memory.h>
+
+int vmap(void* dest, void* phys, size_t size) { int ret; __asm("int $0x95" : "=a" (ret) : "a" (8), "b" (dest), "c" (phys), "d" (size)); return ret; }
+
+uint16_t* const video_mem = (void*)0xA0000000;
+size_t cursor;
+
+void print(const char* s);
+
 void main()
 {
-	int i = 5;
-	i += 2;
+	int res = vmap(video_mem, (void*)0xB8000, 4096);
+	if (res != 0)
+		goto done;
 
+	memset(video_mem, 0, 80*25*sizeof(uint16_t));
+	print("Hello.");
+
+done:
 	for (;;);
+}
+
+void print(const char* s)
+{
+	while (*s != '\0')
+	{
+		video_mem[cursor] = (*s | (0x7 << 8));
+		++cursor, ++s;
+	}
+
+	cursor += 80;
+	cursor -= (cursor % 80);
 }
