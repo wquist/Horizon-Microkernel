0d5e8395793d0f0a780dd1850c1189715cc24ed4
Wyatt Lindquist <git.wquist@gmail.com>
2014-08-26 23:46:57 -0400

    Added a flag for shm calls.

--- lib/libh/include/horizon/shm.h
+++ lib/libh/include/horizon/shm.h
@@ -3,8 +3,10 @@
 
 #include <horizon/types.h>
 
-#define SPROT_READ  0
-#define SPROT_WRITE 1
+#define SPROT_READ  (0 << 0)
+#define SPROT_WRITE (1 << 0)
+
+#define SOPT_FORCE (1 << 0)
 
 struct shm
 {
