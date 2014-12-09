f60ce2ce657a1f157476fbf0f1420d9aa41ee374
Wyatt Lindquist <git.wquist@gmail.com>
2014-12-08 23:14:35 -0500

    Small changes to filesystem.

--- user/vfsd-i586/main.c
+++ user/vfsd-i586/main.c
@@ -1,5 +1,6 @@
 #include <horizon/types.h>
 #include <horizon/ipc.h>
+#include <sys/msg.h>
 #include <stddef.h>
 #include <string.h>
 #include <malloc.h>
@@ -21,7 +22,7 @@ struct inode
 	ftype_t type;
 
 	size_t uid;
-	//ipcport_t driver;
+	ipcport_t driver;
 
 	struct inode* next;
 	struct inode* children;
@@ -38,21 +39,20 @@ struct ftable
 static struct inode root = {0};
 static struct ftable* tables = NULL;
 
-struct inode* find_node(const char* path)
+struct inode* find_node(struct inode* start, const char* path)
 {
-	size_t len = strlen(path);
-	if (len == 1)
-		return (*path == '/') ? &root : NULL;
-
-	char* curr = &path[1];
-	struct inode* node = &root;
+	char* curr = path;
+	struct inode* node = start;
 
 	char* next;
 	do
 	{
+		if (node->type != FTYPE_DIR && node->type != FTYPE_MOUNTPT)
+			return NULL;
+
 		next = strchr(curr, '/');
 
-		char component[64];
+		static char component[64];
 		if (next)
 		{
 			memcpy(component, curr, next-curr);
@@ -64,19 +64,61 @@ struct inode* find_node(const char* path)
 		}
 
 		struct inode* next_node = node->children;
-		node = NULL;
+		struct inode* tmp = NULL;
 
-		while (next_node && !node)
+		while (next_node && !tmp)
 		{
 			if (strcmp(component, next_node->name) == 0)
-				node = next_node;
+				tmp = next_node;
 			else
 				next_node = next_node->next;
 		}
 
-		if (!node)
-			return NULL;
+		if (!tmp)
+		{
+			struct inode* child = malloc(sizeof(struct inode));
+			memset(child, 0, sizeof(struct inode));
+			strcpy(child->name, component);
 
+			if (node->type == FTYPE_MOUNTPT)
+			{
+				child->type = FTYPE_MOUNTPT;
+			}
+			else
+			{
+				struct msg node_request = {{0}};
+				node_request.to = node->driver;
+				node_request.code = VFS_DRQ_FINDDIR;
+
+				node_request.data[0] = node->uid;
+				node_request.payload.buf  = component;
+				node_request.payload.size = strlen(component);
+
+				send(&node_request);
+				wait(node->driver);
+
+				struct msg node_response = {{0}};
+				recv(&node_response);
+
+				if (node_response.code == -1)
+				{
+					free(child);
+					return NULL;
+				}
+
+				child->type = (node_response.data[1]) ? FTYPE_DIR : FTYPE_FILE;
+
+				child->uid = node_response.data[0];
+				child->driver = node->driver;
+			}
+
+			child->next = node->children;
+			node->children = child;
+
+			tmp = child;
+		}
+		
+		node = tmp;
 		curr = next+1;
 	} while (next);
 
