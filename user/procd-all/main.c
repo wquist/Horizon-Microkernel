74dd60b5fbffc8622ba68e3ca1997188a1a594b0
Wyatt Lindquist <git.wquist@gmail.com>
2015-02-28 12:00:04 -0500

    Wrote basic notifier for process deaths.

---
+++ user/procd-all/main.c
@@ -0,0 +1,117 @@
+#include <horizon/types.h>
+#include <horizon/ipc.h>
+#include <sys/sched.h>
+#include <sys/msg.h>
+#include <sys/svc.h>
+#include <malloc.h>
+#include <memory.h>
+#include <stdbool.h>
+
+#include "pnotify.h"
+
+typedef struct pwatch pwatch_t;
+struct pwatch
+{
+	ipcport_t port;
+	unsigned event;
+
+	pwatch_t* next;
+};
+
+typedef struct pstate pstate_t;
+struct pstate
+{
+	pwatch_t* watchers;
+};
+
+pstate_t* states;
+
+void listen(pid_t target, unsigned event, ipcport_t listener)
+{
+	pwatch_t* watch = malloc(sizeof(pwatch_t));
+	watch->port  = listener;
+	watch->event = event;
+
+	watch->next = states[target].watchers;
+	states[target].watchers = watch;
+}
+
+void notify(pid_t changed, unsigned event)
+{
+	pwatch_t* curr = states[changed].watchers;
+	while (curr)
+	{
+		struct msg response = {{0}};
+		response.to = curr->port;
+
+		response.code = event;
+		response.args[0] = changed;
+
+		send(&response);
+
+		pwatch_t* old = curr;
+		curr = curr->next;
+
+		free(old);
+	}
+
+	states[changed].watchers = NULL;
+}
+
+int main()
+{
+	if (svcown(SVC_PROCMGR) < 0)
+		return 1;
+
+	// FIXME: process limit constants.
+	states = malloc(sizeof(pstate_t) * 1024);
+	memset(states, 0, sizeof(pstate_t) * 1024);
+
+	while (true)
+	{
+		wait(IPORT_ANY);
+
+		struct msg msg_in = {{0}};
+		if (recv(&msg_in) < 0)
+		{
+			drop(NULL);
+			continue;
+		}
+
+		switch (msg_in.from)
+		{
+			case IPORT_KERNEL:
+			{
+				notify(msg_in.args[0], PN_ONDEATH);
+				break;
+			}
+			default:
+			{
+				struct msg msg_out = {{0}};
+				msg_out.to = msg_in.from;
+
+				switch (msg_in.code)
+				{
+					// FIXME: check if target process is alive?
+					case PN_LISTEN:
+					{
+						listen(msg_in.args[0], msg_in.args[1], msg_out.from);
+
+						msg_out.code = 0;
+						break;
+					}
+					default:
+					{
+						msg_out.code = -1;
+						break;
+					}
+				}
+
+				send(&msg_out);
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ user/procd-all/pnotify.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum PNOTIFY_REQUEST
+{
+	PN_LISTEN
+};
+
+enum PNOTIFY_EVENT
+{
+	PN_ONDEATH
+};
