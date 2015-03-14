8002876403b37caba8b67e837627b4217e9d90b0
Wyatt Lindquist <git.wquist@gmail.com>
2015-03-13 23:54:34 -0400

    IDE driver now works as device.

--- user/idehdd-all/main.c
+++ user/idehdd-all/main.c
@@ -5,94 +5,79 @@
 #include <sys/proc.h>
 #include <stdbool.h>
 #include <string.h>
+#include <malloc.h>
 
 #include "idectl.h"
 #include "../vfsd-all/fs.h"
 
-ipcport_t filesystem;
-int screen;
+ipcport_t devmgr;
 
-int open(const char* path)
+bool register_device(char* name)
 {
 	struct msg request = {{0}};
-	request.to = filesystem;
-
-	request.code = VFS_OPEN;
-	request.payload.buf  = (void*)path;
-	request.payload.size = strlen(path)+1;
+	request.to = devmgr;
+	request.code = 100;
+	request.payload.buf = name;
+	request.payload.size = strlen(name);
 
 	send(&request);
-	wait(filesystem);
+	wait(devmgr);
 
 	struct msg response = {{0}};
 	recv(&response);
+	if (response.code != 0)
+		return false;
 
-	return response.code;
-}
-
-int write(int fd, char* buffer, size_t size)
-{
-	struct msg request = {{0}};
-	request.to = filesystem;
-
-	request.code = VFS_WRITE;
-	request.args[0] = fd;
-	request.args[1] = size;
-
-	request.payload.buf  = buffer;
-	request.payload.size = size;
-
-	send(&request);
-	wait(filesystem);
-
-	struct msg response = {{0}};
-	recv(&response);
-
-	return response.code;
-}
-
-void print(char* msg)
-{
-	write(screen, msg, strlen(msg)+1);
+	return true;
 }
 
 int main()
 {
-	while ((filesystem = svcid(SVC_VFS)) == 0);
+	ipcport_t devmgr;
+	while ((devmgr = svcid(SVC_DEVMGR)) == 0);
 
-	while ((screen = open("/dev/tty")) == -1);
-	print("Connected to TTY.\n");
-
-	print("Identifying IDE devices... ");
 	ide_controller_t ctl = { .base = 0x1F0 };
 	idectl_identify(&ctl, IDE_MASTER);
-	print("OK!\n");
 
-	if (ctl.devices[IDE_MASTER].present)
-		print("Found an IDE device.\n");
-	else
+	if (!(ctl.devices[IDE_MASTER].present))
+		return 1;
+	if (!register_device("ata0"))
 		return 1;
 
-	print("Device model: ");
-	print(ctl.devices[IDE_MASTER].model);
-	print("\nDevice serial: ");
-	print(ctl.devices[IDE_MASTER].serial);
-	print("\nDevice firmware: ");
-	print(ctl.devices[IDE_MASTER].firmware);
-	print("\n\n");
-
-	print("Reading first block of disk... ");
-
-	char buf[512];
-	idectl_block_io(&ctl, IDE_MASTER, IDE_READ, 0, 1, buf);
-	print("OK!\n");
-	
-	for (size_t i = 0; i != 256; ++i)
+	void* buffer = malloc(512);
+	while (true)
 	{
-		char str[] = { buf[i], '\0' };
-		print(str);
+		wait(IPORT_ANY);
+
+		struct msg request = {{0}};
+		request.payload.buf  = buffer;
+		request.payload.size = 512;
+
+		struct msg response = {{0}};
+		response.to = request.from;
+		switch (request.code)
+		{
+			case 1:
+			{
+				size_t off = request.args[1];
+				idectl_block_io(&ctl, IDE_MASTER, IDE_READ, off, 1, buffer);
+
+				response.code = 0;
+				response.payload.buf  = buffer;
+				response.payload.size = 512;
+
+				send(&response);
+				break;
+			}
+			default:
+			{
+				response.code = -1;
+
+				send(&response);
+				break;
+			}
+		}
 	}
 
-	for (;;);
 	return 0;
 }
